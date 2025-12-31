798d112c1e7f15c815d2fff186ba62cdce13f3e3
Nv7-Github <junk4Nv7@gmail.com>
2025-12-30 23:28:09 -0800

    Try to make it work (failed)

--- src/main.cpp
+++ src/main.cpp
@@ -9,8 +9,8 @@ RCPWMReader chanB(3);
 RCPWMReader chanC(5);
 RCPWMReader chanD(7);
 
-DShot::ESC dshot(6, pio0, DShot::Type::Bidir, DShot::Speed::DS600, 10);
-DShot::ESC dshot(8, pio0, DShot::Type::Bidir, DShot::Speed::DS600, 10);
+DShot::ESC dshot1(6, pio0, DShot::Type::Bidir, DShot::Speed::DS600, 10);
+DShot::ESC dshot2(8, pio0, DShot::Type::Bidir, DShot::Speed::DS600, 10);
 
 void setup() {
   Serial.begin(115200);
@@ -20,7 +20,8 @@ void setup() {
   chanB.begin();
   chanC.begin();
   chanD.begin();
-  dshot.init();
+  dshot1.init();
+  dshot2.init();
 }
 
 void printChan(RCPWMReader chan, const char* name, const char* start) {
@@ -31,11 +32,42 @@ void printChan(RCPWMReader chan, const char* name, const char* start) {
                 pulse);
 }
 
+void getTelemetry(DShot::ESC& esc, const char* name) {
+  uint64_t raw_telemetry;
+  if (esc.getRawTelemetry(raw_telemetry)) {
+    DShot::Telemetry telemetry;
+    if (esc.decodeTelemetry(raw_telemetry, telemetry)) {
+      Serial.printf(
+          ",%s_temp:%.1f,%s_voltage:%.2f,%s_current:%.2f,%s_erpm:%.2f", name,
+          telemetry.temperature_C, name, telemetry.volts_cV, name,
+          telemetry.amps_A, name, telemetry.eRPM_period_us);
+    }
+  }
+}
+
 void loop() {
+  // Init DShot
+  if (millis() < 3000) {
+    dshot1.setCommand(0);  // 1046 is the example command
+    dshot2.setCommand(0);
+  } else if (millis() < 4000) {
+    dshot1.setCommand(13);  // extended telemetry enable
+    dshot2.setCommand(13);  // extended telemetry enable
+  } else {
+    dshot1.setThrottle(
+        0.25);  // https://github.com/betaflight/betaflight/issues/2879
+    dshot2.setThrottle(
+        0.25);  // https://github.com/betaflight/betaflight/issues/2879
+  }
+
   printChan(chanA, "A", "");
   printChan(chanB, "B", ",");
   printChan(chanC, "C", ",");
   printChan(chanD, "D", ",");
+
+  // Check for telemetry
+  getTelemetry(dshot1, "esc1");
+  getTelemetry(dshot2, "esc2");
   Serial.println();
   delay(50);
 }
